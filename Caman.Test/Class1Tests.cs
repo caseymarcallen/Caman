d0782865df53e9c85d399929d581cec89c88e8c9
Casey Allen <caseymarcallen@gmail.com>
2014-09-08 23:10:25 +0800

    Adding a passing test
    Want to test out running tests on commits and getting a CI environment
    setup so adding a dummy test.

---
+++ Caman.Test/Class1Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Caman.Test
+{
+	public class Class1Tests
+    {
+		[Fact]
+		public void Class1_Exists()
+		{
+			var c1 = new Class1();
+			Assert.IsType<Class1>(c1);
+		}
+    }
+}
